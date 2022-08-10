efd15ae041a8fc5fee2a488bb0b369133fa32495
Stephen <stvlley.amzn@gmail.com>
2022-08-10 11:37:34 -0400

    thank  you progmatic programmers

---
+++ wordBuilder.js
@@ -0,0 +1,19 @@
+
+
+const array = ['a', 'b', 'c', 'd']
+
+
+const wordBuilder = (array) => {
+    let collection = [];
+
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < array.length; j ++){
+            if( i !== j) {
+                collection.push(array[i] + array[j])
+            }
+        }
+    }
+    return collection
+}
+
+console.log(wordBuilder(array))
